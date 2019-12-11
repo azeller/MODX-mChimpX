0e3b3fa429f88029793450309ee31d60797231ff
azeller <zeller@lux-medien.com>
2019-12-11 13:59:20 +0100

    German Translation for system and error messages
    Added a German translation, so all the fields will also be pretty much plug and play for German users.

---
+++ core/components/mchimpx/lexicon/de/default.inc.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * mChimpX
+ *
+ * German translation by lux-medien.com
+ *
+ * Copyright 2011-2012 by Bert Oost at OostDesign.nl <bert@oostdesign.nl>
+ *
+ * mChimpX is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option) any
+ * later version.
+ *
+ * mChimpX is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * mChimpX; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package mchimpx
+ */
+/**
+ * The German translation of the original lexicon language entries
+ *
+ * @package mchimpx
+ * @subpackage lexicon
+ */
+
+$_lang['mchimpx'] = "Mailchimp für MODx";
+
+$_lang['mchimpx.error.unknown'] = "Ein unbekannter Fehler ist aufgetreten!";
+$_lang['mchimpx.error.noapi'] = "Kein API-Schlüssel für Mailchimp gefunden!";
+$_lang['mchimpx.error.nolistid'] = "Keine Mailchimp Listen-ID definiert!";
+$_lang['mchimpx.error.noemail'] = "Keine Mailchimp Mailadresse definiert!";
+$_lang['mchimpx.error.nomergefields'] = "Mailchimp mergeTags Variable fehlerhaft!";
+
+// Mailchimp errors
+$_lang['mchimpx.error.invalidapikey'] = "Mailchimp API Schlüssel falsch konfiguriert!";
+$_lang['mchimpx.error.listnotexists'] = "Mailchimp Listen-ID falsch konfiguriert!";
+$_lang['mchimpx.error.alreadysubscribed'] = "Sie haben den Newsletter bereits abonniert.";
+$_lang['mchimpx.error.notsubscribed'] = "Fehler beim Abonnieren!";
+$_lang['mchimpx.error.missingrequired'] = "Bitte alle Pflichtfelder ausfüllen!";
+$_lang['mchimpx.error.mailchimp_error'] = "MailChimp Fehler!";
+$_lang['mchimpx.error.system_error'] = "Mailchimp Systemfehler!";
+
